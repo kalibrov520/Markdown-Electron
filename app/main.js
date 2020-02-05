174523e95e7bf995a0f0f34433ac60cf2bd9f3c7
kalibrov520 <kalibrov520@gmail.com>
2020-02-05 15:10:07 +0300

    initial electron app creation

---
+++ app/main.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const { app, BrowserWindow, dialog } = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({ show: false });
+
+    mainWindow.loadFile(`${__dirname}/index.html`);
+
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();  
+    });
+});
+
+exports.getFileFromUser = () => {
+    const files = dialog.showOpenDialog({
+        properties: ['openFile'],
+        title: 'Open Fire Sale Document',
+        filters: [
+            { name: 'Markdown Files', extensions: ['md', 'mdown']},
+            { name: 'Text Files', extensions: ['txt', 'text']}
+        ]
+    });
+
+    if (!files) return;
+
+    const file = files[0];
+
+    openFile(file);
+};
+
+const openFile = (file) => {
+    const content = fs.readFileSync(file).toString();
+
+    mainWindow.webContents.send('file-opened', file, content);
+};
+
+
